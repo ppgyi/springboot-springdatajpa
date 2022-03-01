aaa99e84679e1af2b4e387cd942907138283e0bb
Peter <peter.palagyi@gmail.com>
2022-03-01 14:07:01 +0100

    Request param http://localhost:8080/param?name=Peter

--- src/main/java/com/example/springbootspringdatajpa/logika/HelloController.java
+++ src/main/java/com/example/springbootspringdatajpa/logika/HelloController.java
@@ -1,20 +1,24 @@
 package com.example.springbootspringdatajpa.logika;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-// Vramci MVC je toto ta vrchna vrstva Controller, v strede je ta logika
-// a na konci je databaza
 
 @RestController
 public class HelloController {
 
-    // Get request nema mat telo, nikdy...
-    // odpovet kod 200 je ok...400 je na figu, ale je to chyba v mojom kode
-    // kody najdem cez google...500 kody su chyby servera
+    private int counter = 0;
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world" + (this.counter++);
+    }
+
+    // http://localhost:8080/param?name=Peter
+    // ak nedame nic tak da defaultvalue
+    @GetMapping("/param")
+    public String helloParam(@RequestParam(
+            name = "name", defaultValue = "WORLD") String name){
+        return "Hello " + name;
     }
 }

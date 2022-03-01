6d9f2af64b25628867efe3cd0c3801c00a1cf0f5
Peter <peter.palagyi@gmail.com>
2022-03-01 10:30:21 +0100

    initial commit

---
+++ src/test/java/com/example/springbootspringdatajpa/SpringbootSpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringdatajpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

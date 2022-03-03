ed576b4e5697bc598f9e01adf49335fe6fc572b6
Peter <peter.palagyi@gmail.com>
2022-03-03 14:14:21 +0100

    animal response with person object

--- src/main/java/com/example/springbootspringdatajpa/response/TestAnimalResponse.java
+++ src/main/java/com/example/springbootspringdatajpa/response/TestAnimalResponse.java
@@ -1,6 +1,7 @@
 package com.example.springbootspringdatajpa.response;
 
 import com.example.springbootspringdatajpa.entity.TestEntityAnimal;
+import com.example.springbootspringdatajpa.entity.TestEntityPerson;
 import lombok.Getter;
 
 @Getter
@@ -12,12 +13,14 @@ public class TestAnimalResponse {
 
     private String name;
 
-    private Long person;
+//    private Long person;
+    private TestPersonResponse person;
 
     public TestAnimalResponse(TestEntityAnimal a) {
         this.id = a.getId();
         this.species = a.getSpecies();
         this.name = a.getName();
-        this.person = a.getPerson() == null ? null : a.getPerson().getId();
+//        this.person = a.getPerson() == null ? null : a.getPerson().getId();
+        this.person = a.getPerson() != null ? new TestPersonResponse(a.getPerson()) : null;
     }
 }
